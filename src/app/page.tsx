f6a6764e9050b71367717170f6bb58a2ec178985
franmartos11 <franmartos11@gmail.com>
2023-11-30 03:03:42 -0300

    fix deployment on component that needed use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
+"use client"
 import Contact from "@/Components/Contact/Contact";
 import DobleImg from "@/Components/DobleImagenTexto/DobleImg";
 import Nav from "@/Components/HeaderNav/Nav";
-import HomePresentationV2 from "@/Components/HomePresentationV2/HomePresentationV2";
 import ImageWithContent from "@/Components/ImageWithContent/ImageWithContent";
 import AboutUsHEADERv3 from "@/Components/about-us-header/AboutUsHeaderV3";
 import Card from "@/Components/card/Card";
 import HorizontalScrollCarousel from "@/Components/carrusel/HorizontalScrollCarousel";
-import DatosInteres from "@/Components/datosInteres/DatosInteres";
 import Footer from "@/Components/footer/Footer";
-import HomePresentation from "@/Components/home-presentation/HomePresentation";
 import ServicesDisplay from "@/Components/servicesDisplay/ServicesDisplay";
-import Image from "next/image";
+
 
 export default function Home() {
     return (
-        <div>
+        <>
             <Nav></Nav>
             <AboutUsHEADERv3></AboutUsHEADERv3>
             <HorizontalScrollCarousel></HorizontalScrollCarousel>
@@ -25,6 +23,6 @@ export default function Home() {
             <Contact></Contact>
             
             <Footer></Footer>
-        </div>
+        </>
     );
 }

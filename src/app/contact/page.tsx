d2efac640088fd4792d4ed18377aa743ac77492f
franmartos11 <franmartos11@gmail.com>
2023-12-01 22:36:38 -0300

    contact us added and also links on nav and responsive nav

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Contact from "@/Components/Contact/Contact";
+import ContactCard from "@/Components/Contact/ContactCard";
+import ContactCardImg from "@/Components/Contact/ContactCardImg";
+
+export default function ContactUs(){
+    return(
+        <div>
+            <ContactCardImg></ContactCardImg>
+        </div>
+    )
+}

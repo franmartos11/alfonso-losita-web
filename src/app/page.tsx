c9e2246b83877ace75c7ac2d6261a9f7374c6549
franmartos11 <franmartos11@gmail.com>
2023-12-06 14:55:48 -0300

    changes for production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import AboutUsHEADERv3 from "@/Components/about-us-header/AboutUsHeaderV3";
 import CarrouselMamado from "@/Components/carrouselScroll/CarrouselMamado";
 import HorizontalScrollCarousel from "@/Components/carrusel/HorizontalScrollCarousel";
 import ServicesDisplay from "@/Components/servicesDisplay/ServicesDisplay";
-import WhatsAppButton from "@/Components/whatsApp-pop/WhatsappPop";
+
 
 export default function Home() {
     return (
